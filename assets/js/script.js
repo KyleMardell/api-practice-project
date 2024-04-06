21e23973988fdea8c7fdaf75721cf2ea8cb9777e
Kyle Mardell <151871777+KyleMardell@users.noreply.github.com>
2024-04-06 23:03:40 +0100

    Add code to display pokemon info and image

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,9 +20,22 @@ const findPokemon = () => {
             type: data.types.map(type => type.type.name).join(", ")
         };
     
-        console.log(pokemon);
+        displayPokemon(pokemon);
     })
     .catch((error) => console.log(error));
 };
+
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
+    const infoHTML = `
+        <h3>Name: ${pokemon.name}</h3>
+        <h4>ID#: ${pokemon.id}</h4>
+        <p>Types: ${pokemon.type}<p>
+    `;
+    const imageHTML = `<img src="${pokemon.image}" alt="Image of ${pokemon.name}">`;
+    
+    document.getElementById("pokemon-info").innerHTML = infoHTML;
+    document.getElementById("pokemon-image").innerHTML = imageHTML;
+}
 
 document.getElementById("search-btn").addEventListener("click", findPokemon);
