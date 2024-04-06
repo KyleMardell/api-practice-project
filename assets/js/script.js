7f8fbb0777ce6b328e443b92b9185d0ef8ff1772
Kyle Mardell <151871777+KyleMardell@users.noreply.github.com>
2024-04-06 22:39:12 +0100

    Add api fetch function and create the initial pokemon object for testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,26 @@
-console.log("hello world");
+// Pokemon search
+
+const findPokemon = () => { 
+    const url = `https://pokeapi.co/api/v2/pokemon/1`;
+    fetch(url)
+        .then(response => {
+            if(!response.ok){
+                throw new Error("Pokemon not found")
+            } else {
+                return response.json();
+            }
+    })
+    .then(data => {
+        console.log(data);
+        const pokemon = {
+            name: data.name,
+            id: data.id,
+            image: data.sprites.front_default,
+        };
+    
+        console.log(pokemon);
+    })
+    .catch((error) => console.log(error));
+};
+
+findPokemon();
