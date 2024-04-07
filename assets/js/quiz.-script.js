7eab174d54f5eed6247cc9809bb1e88211a89554
Kyle Mardell <151871777+KyleMardell@users.noreply.github.com>
2024-04-07 11:11:38 +0100

    Add code to fetch a question from the api and log it to the console

--- assets/js/quiz.-script.js
+++ assets/js/quiz.-script.js
@@ -1,1 +1,26 @@
-console.log("hello world");
+
+const getQuestion = () => {
+    const url = `https://opentdb.com/api.php?amount=1`;
+
+    // Use fetch to get question from open trivia database
+    fetch(url)
+        .then(response => {
+            // Check fetch was sent correctly
+            if (!response.ok) {
+                throw new Error("Not found")
+            } else {
+                return response.json();
+            }
+        })
+        // Create question
+        .then(data => {
+            console.log(data);
+        })
+        // Alert the user of the error and log to console
+        .catch(error => {
+            alert("An error has occurred!");
+            console.log(error);
+        });
+};
+
+getQuestion();
