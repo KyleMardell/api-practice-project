6a78517d341075effa0e1adae2d749b08266b70b
Kyle Mardell <151871777+KyleMardell@users.noreply.github.com>
2024-04-07 12:27:25 +0100

    Add code to checkAnswer function to load a new question

--- assets/js/quiz.-script.js
+++ assets/js/quiz.-script.js
@@ -26,24 +26,44 @@ const getQuestion = () => {
 };
 
 const displayQuestion = (data) => {
+    const answers = [
+        data.results[0].correct_answer,
+        data.results[0].incorrect_answers[0],
+        data.results[0].incorrect_answers[1],
+        data.results[0].incorrect_answers[2]
+    ];
+
+    answers.sort(() => Math.random() - 0.5);
+
     document.getElementById("question").innerText = `Question: ${data.results[0].question}`;
-    document.getElementById("answer1").innerText = data.results[0].correct_answer;
-    document.getElementById("answer2").innerText = data.results[0].incorrect_answers[0];
-    document.getElementById("answer3").innerText = data.results[0].incorrect_answers[1];
-    document.getElementById("answer4").innerText = data.results[0].incorrect_answers[2];
+    document.getElementById("answer1").innerText = answers[0];
+    document.getElementById("answer2").innerText = answers[1];
+    document.getElementById("answer3").innerText = answers[2];
+    document.getElementById("answer4").innerText = answers[3];
 };
 
 const checkAnswer = (data) => {
     let correctAnswer = data.results[0].correct_answer;
     let answerButtons = document.getElementsByTagName("button");
-    for (let button of answerButtons) {
-        button.addEventListener("click", function () {
+    let questionAnswered = false;
+
+    function buttonClicked(event) {
+        const button = event.target;
+        if (!questionAnswered) {
             if (button.innerText === correctAnswer) {
                 alert("That is correct!");
+                questionAnswered = true;
+                getQuestion();
             } else {
                 alert("Sorry, that is incorrect");
+                questionAnswered = true;
+                getQuestion();
             }
-        })
+        }
+    }
+
+    for(let button of answerButtons){
+        button.addEventListener("click", buttonClicked);
     }
 };
 
