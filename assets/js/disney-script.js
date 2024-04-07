b45f7c8c230ee379b55c0a26b2c60cfb938e7063
Kyle Mardell <151871777+KyleMardell@users.noreply.github.com>
2024-04-07 16:41:30 +0100

    Add code to get a list of all disney characters and display in html

--- assets/js/disney-script.js
+++ assets/js/disney-script.js
@@ -1,1 +1,40 @@
-console.log("hello world");
+const getAllCharacters = () => {
+    const url = `https://api.disneyapi.dev/character?pageSize=7438`;
+
+    // Use fetch to get all characters from the api
+    fetch(url)
+        .then(response => {
+            // Check fetch was sent correctly
+            if (!response.ok) {
+                throw new Error("Not found")
+            } else {
+                return response.json();
+            }
+        })
+        // Create disney data
+        .then(data => {
+            console.log(data);
+            displayCharacterList(data);
+        })
+        // Alert the user of the error and log to console
+        .catch(error => {
+            alert("An error has occurred!");
+            console.log(error);
+        });
+};
+
+getAllCharacters();
+
+const displayCharacterList = (data) => {
+    const characterList = document.getElementById("character-list");
+    let characterHTML = "";
+
+    for (let i = 0; i < 7438; i++) {
+        characterHTML += `
+            <li>
+                ${data.data[i].name}
+            </li>
+        `
+    }
+    characterList.innerHTML = characterHTML;
+};
