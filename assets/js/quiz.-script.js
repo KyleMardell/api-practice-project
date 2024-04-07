40aae40a1dcda188d1d96c22c1c88d4e8f99d603
Kyle Mardell <151871777+KyleMardell@users.noreply.github.com>
2024-04-07 11:24:08 +0100

    Add code to display question and answers, change api link to single multiple choice question

--- assets/js/quiz.-script.js
+++ assets/js/quiz.-script.js
@@ -1,6 +1,6 @@
 // Get a question from the api
 const getQuestion = () => {
-    const url = `https://opentdb.com/api.php?amount=1`;
+    const url = `https://opentdb.com/api.php?amount=1&type=multiple`;
 
     // Use fetch to get question from open trivia database
     fetch(url)
@@ -15,6 +15,7 @@ const getQuestion = () => {
         // Create question
         .then(data => {
             console.log(data);
+            displayQuestion(data);
         })
         // Alert the user of the error and log to console
         .catch(error => {
@@ -22,5 +23,13 @@ const getQuestion = () => {
             console.log(error);
         });
 };
+
+const displayQuestion = (data) => {
+    document.getElementById("question").innerText = `Question: ${data.results[0].question}`;
+    document.getElementById("answer1").innerText = data.results[0].correct_answer;
+    document.getElementById("answer2").innerText = data.results[0].incorrect_answers[0];
+    document.getElementById("answer3").innerText = data.results[0].incorrect_answers[1];
+    document.getElementById("answer4").innerText = data.results[0].incorrect_answers[2];
+}
 
 document.addEventListener("DOMContentLoaded", getQuestion);
