850ae4c0cfc7cdb3b0441d9fc649a68302736ddc
Kyle Mardell <151871777+KyleMardell@users.noreply.github.com>
2024-04-09 21:28:43 +0100

    Add code to create api url from user input form in quiz

--- assets/js/quiz.-script.js
+++ assets/js/quiz.-script.js
@@ -1,57 +1,3 @@
-async function fetchQuestions(url) {
-    try {
-        const response = await fetch(url);
-        if(!response.ok){
-            throw "Error";
-        }
-
-        const data = await response.json();
-        console.log(data);
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-function getUserQuestions() {
-    const chosenQuestionAmount = document.getElementById("questions-amount");
-    
-}
-
-
-
-
-
-
-/**
- * Get a new question from the api
- */
-const getQuestion = () => {
-    const url = `https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple`;
-
-    // Use fetch to get question from open trivia database
-    fetch(url)
-        .then(response => {
-            // Check fetch was sent correctly
-            if (!response.ok) {
-                throw new Error("Not found")
-            } else {
-                return response.json();
-            }
-        })
-        // Create question
-        .then(data => {
-            console.log(data);
-            displayQuestion(data);
-            checkAnswer(data);
-        })
-        // Alert the user of the error and log to console
-        .catch(error => {
-            alert("An error has occurred!");
-            console.log(error);
-        });
-};
-
-
 /**
  * Get the answers to the question and put them in a randome order
  * then display them in the quiz html 
@@ -73,35 +19,147 @@ const displayQuestion = (data) => {
     document.getElementById("answer4").innerText = answers[3];
 };
 
-/**
- * Check if an answer button has been clicked
- * and check if the answer is correct
- */
-const checkAnswer = (data) => {
-    let correctAnswer = data.results[0].correct_answer;
-    let answerButtons = document.getElementsByClassName("answer-btn");
-    let questionAnswered = false;
-
-    function buttonClicked(event) {
-        const button = event.target;
-        if (!questionAnswered) {
-            if (button.innerText === correctAnswer) {
-                alert("That is correct!");
-                questionAnswered = true;
-                getQuestion();
-            } else {
-                alert("Sorry, that is incorrect");
-                questionAnswered = true;
-                getQuestion();
-            }
+async function fetchQuestions(url) {
+    try {
+        const response = await fetch(url);
+        
+        if(!response.ok){
+            throw "Error";
         }
+
+        const data = await response.json();
+        console.log(data);
+        displayQuestion(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function getUserQuestions() {
+    const chosenQuestionAmount = document.getElementById("questions-amount").value;
+    const chosenDifficulty = document.querySelector('input[name="difficulty"]:checked').id;
+    const chosenCategory = document.querySelector('input[name="category"]:checked').id;
+    return [chosenQuestionAmount, chosenCategory, chosenDifficulty];
+}
+
+function createURL(chosenArray) {
+    // https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple
+
+    const amount = `amount=${chosenArray[0]}`;
+
+    const chosenCategory = chosenArray[1];
+    let category = "";
+
+    switch(chosenCategory){
+        case "cat-any": 
+            category = "";
+            break;
+        case "cat-gen":
+            category = "&category=9";
+            break;
+        case "cat-film":
+            category = "&category=11";
+            break;
+        case "cat-music":
+            category = "&category=12";
+            break;
+        case "cat-sport":
+            category = "&category=21";
+            break;
+        default:
+            category = "";
     }
 
-    // Loop though the answer buttons, apply the buttonClicked function to each one
-    for(let button of answerButtons){
-        button.addEventListener("click", buttonClicked);
-    }
-};
+    const chosenDifficulty = chosenArray[2];
+    let difficulty = "";
 
-// Get a new question once the dom has been loaded
-document.addEventListener("DOMContentLoaded", getQuestion);
+    switch(chosenDifficulty) {
+        case "dif-easy":
+            difficulty = "$difficulty=easy";
+            break;
+        case "dif-med":
+            difficulty = "&difficulty=medium";
+            break;
+        case "dif-hard":
+            difficulty = "&difficulty=hard";
+            break;
+        default:
+            difficulty = "$difficulty=easy";
+    }
+
+    return `https://opentdb.com/api.php?${amount}${category}${difficulty}`;
+}
+
+
+document.getElementById("quiz-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const formData = getUserQuestions();
+    const url = createURL(formData);
+    console.log(formData, url);
+    fetchQuestions(url);
+});
+
+
+
+
+// /**
+//  * Get a new question from the api
+//  */
+// const getQuestion = () => {
+//     const url = `https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple`;
+
+//     // Use fetch to get question from open trivia database
+//     fetch(url)
+//         .then(response => {
+//             // Check fetch was sent correctly
+//             if (!response.ok) {
+//                 throw new Error("Not found")
+//             } else {
+//                 return response.json();
+//             }
+//         })
+//         // Create question
+//         .then(data => {
+//             console.log(data);
+//             displayQuestion(data);
+//             checkAnswer(data);
+//         })
+//         // Alert the user of the error and log to console
+//         .catch(error => {
+//             alert("An error has occurred!");
+//             console.log(error);
+//         });
+// };
+
+// /**
+//  * Check if an answer button has been clicked
+//  * and check if the answer is correct
+//  */
+// const checkAnswer = (data) => {
+//     let correctAnswer = data.results[0].correct_answer;
+//     let answerButtons = document.getElementsByClassName("answer-btn");
+//     let questionAnswered = false;
+
+//     function buttonClicked(event) {
+//         const button = event.target;
+//         if (!questionAnswered) {
+//             if (button.innerText === correctAnswer) {
+//                 alert("That is correct!");
+//                 questionAnswered = true;
+//                 getQuestion();
+//             } else {
+//                 alert("Sorry, that is incorrect");
+//                 questionAnswered = true;
+//                 getQuestion();
+//             }
+//         }
+//     }
+
+//     // Loop though the answer buttons, apply the buttonClicked function to each one
+//     for(let button of answerButtons){
+//         button.addEventListener("click", buttonClicked);
+//     }
+// };
+
+// // Get a new question once the dom has been loaded
+// document.addEventListener("DOMContentLoaded", getQuestion);
